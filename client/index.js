417a260c80acf8f3c134b36a57c0ec8d1a2f1b14
Scott Soffel <ssoffel@hotmail.com>
2018-05-03 15:50:04 -0400

    finished map key

--- client/index.js
+++ client/index.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+const contacts = [
+  {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
+  {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
+  {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
+]
+
 class Main extends Component {
+  constructor(){
+    super()
+    this.state = { contacts: contacts}
+  }
+
   render () {
     return (
       <div id='main'>
@@ -9,13 +20,32 @@ class Main extends Component {
           <div>Contact List</div>
         </div>
         <div id='container'>
-          {/* your components here */}
-        </div>
+           <table>
+             <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+              </tr>
+
+              {this.state.contacts.map(element => {
+                return (
+              <tr>
+                <td key={element.id}>{element.name}</td>
+                <td key={element.id}>{element.phone}</td>
+                <td key={element.id}>{element.email}</td>
+              </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </div>
       </div>
     )
   }
-}
 
+}
 ReactDOM.render(
   <Main />,
   document.getElementById('app')
